d002d596d7dad7bfa3ea156df426bd420eedb1a8
ikitsuchi <ikitsuchi@outlook.com>
2021-10-28 21:00:10 +0800

    Optimized the way to check the convergence

--- kmeans.cpp
+++ kmeans.cpp
@@ -4,6 +4,11 @@
 #include <limits>
 #include <queue>
 #include <thread>
+#include <chrono>
+
+using std::chrono::duration_cast;
+using std::chrono::high_resolution_clock;
+using std::chrono::microseconds;
 
 inline double Point::Distance(const Point &other) const noexcept {
   double a = x - other.x;
@@ -34,11 +39,14 @@ std::vector<index_t> Kmeans::Run(int max_iterations) {
             << ", num centers = " << m_numCenters
             << ", max iterations = " << max_iterations << "...\n";
 
+  double loop_1 = 0, loop_2 = 0;
+
   std::vector<int> p_cnt(m_numCenters, 0);
   while (max_iterations--) {
     ++curr_iteration;
-    std::vector<index_t> assignment_bak = assignment;
+    bool changed = false;
 
+    //auto start = high_resolution_clock::now();
     #pragma omp parallel for
     for (int i = 0; i < m_numPoints; ++i) {
       Point &p_i = m_points[i];
@@ -52,22 +60,30 @@ std::vector<index_t> Kmeans::Run(int max_iterations) {
           ans = k;
         }
       }
-      assignment[i] = ans;
+      if (ans != assignment[i]) {
+        assignment[i] = ans;
+        changed = true;
+      }
     }
-
-    if (assignment == assignment_bak) {
+    //auto end = high_resolution_clock::now();
+    //loop_1 += (double) duration_cast<microseconds>(end - start).count() / 1000000.0;
+    
+    if (!changed) {
       goto converge;
     }
 
     m_centers.assign(m_numCenters, Point());
     p_cnt.assign(m_numPoints, 0);
 
+    //start = high_resolution_clock::now();
     for (int i = 0; i < m_numPoints; ++i) {
-      index_t &cluster_1 = assignment[i];
+      index_t cluster_1 = assignment[i];
       m_centers[cluster_1].x += m_points[i].x;
       m_centers[cluster_1].y += m_points[i].y;
-      p_cnt[cluster_1]++;
+      ++p_cnt[cluster_1];
     }
+    //end = high_resolution_clock::now();
+    //loop_2 += (double) duration_cast<microseconds>(end - start).count() / 1000000.0;
 
     for (int j = 0; j < m_numCenters; ++j) {
       m_centers[j].x /= p_cnt[j];
@@ -77,5 +93,7 @@ std::vector<index_t> Kmeans::Run(int max_iterations) {
 
 converge:
   std::cout << "Finished in " << curr_iteration << " iterations." << std::endl;
+  //std::cout << "Loop_1 time cost: " << loop_1 << "s" << std::endl;
+  //std::cout << "Loop_2 time cost: " << loop_2 << "s" << std::endl;
   return assignment;
 }
